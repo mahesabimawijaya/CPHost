0e7105894f4c2189c3b4beddd8e379244ee4407f
Mahesa Wijaya <mahesabw26@gmail.com>
2024-10-28 10:52:20 +0700

    feat: transaction with nodemailer

--- apps/cms/src/api/transaction/controllers/transaction.ts
+++ apps/cms/src/api/transaction/controllers/transaction.ts
@@ -2,6 +2,68 @@
  * transaction controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::transaction.transaction');
+export default factories.createCoreController(
+  "api::transaction.transaction",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const { data } = ctx.request.body;
+      const { domain, invoice, endDate, amount, templateId, planId, user } =
+        data;
+
+      if (
+        !domain ||
+        !invoice ||
+        !endDate ||
+        !amount ||
+        !templateId ||
+        !planId ||
+        !user
+      ) {
+        return ctx.badRequest("Please provide all required fields");
+      }
+
+      data.uuid = crypto.randomUUID();
+
+      const plan = await strapi.entityService.findOne("api::plan.plan", planId);
+
+      if (!plan) {
+        return ctx.notFound("Plan not found");
+      }
+
+      const template = await strapi.entityService.findOne(
+        "api::template.template",
+        templateId,
+      );
+
+      if (!template) {
+        return ctx.notFound("Template not found");
+      }
+
+      const transaction = await super.create(ctx);
+
+      const token = await strapi
+        .service("api::transaction.transaction")
+        .createPayment(data.uuid, amount, {
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        });
+
+      return ctx.send({
+        message: "Transaction created successfully",
+        token, // Send Snap token back to frontend
+        transaction,
+      });
+    },
+
+    async handlePaymentNotification(ctx) {
+      const notification = ctx.request.body;
+      const response = await strapi
+        .service("api::transaction.transaction")
+        .handlePaymentNotification(notification);
+      return ctx.send(response);
+    },
+  }),
+);
--- apps/cms/src/api/transaction/routes/transaction.ts
+++ apps/cms/src/api/transaction/routes/transaction.ts
@@ -2,6 +2,6 @@
  * transaction router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::transaction.transaction');
+export default factories.createCoreRouter("api::transaction.transaction");
