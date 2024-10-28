29defb234fa115ef9fa07f79403533459575e51a
astnab <astnab@gmail.com>
2024-10-28 15:05:57 +0700

    feat: auth service, tawkto setup

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { SECRET_KEY } from 'src/configs/config';
+// import { ProductModule } from './product/product.module';
+// import { TransactionModule } from './transaction/transaction.module';
+// import { MidtransService } from './midtrans/midtrans.service';
+// import { MidtransModule } from './midtrans/midtrans.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UserModule,
+    // ProductModule,
+    // TransactionModule,
+    JwtModule.register({
+      secret: SECRET_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+    // MidtransModule,
+  ],
+  // providers: [MidtransService],
 })
 export class AppModule {}
